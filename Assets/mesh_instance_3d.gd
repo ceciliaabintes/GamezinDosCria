1ea5d7ba285e86991861b3997514dca88c7d0dfd
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2025-05-19 10:26:45 -0300

    Update mesh_instance_3d.gd

--- Assets/mesh_instance_3d.gd
+++ Assets/mesh_instance_3d.gd
@@ -4,3 +4,11 @@ extends MeshInstance3D
 var aceleration = Vector3.ZERO
 if Input.is_key_pressed(KEY_A):
 	print("Tecla A está pressionada")
+if Input.is_key_pressed(KEY_D):
+    print("Tecla D está pressionada")
+if Input.is_key_pressed(KEY_W):
+    print("Tecla W está pressionada")
+if Input.is_key_pressed(KEY_S):
+    print("Tecla S está pressionada")
+if Input.is_key_pressed(KEY_SPACE): 
+    print("Tecla SPACE está pressionada")
